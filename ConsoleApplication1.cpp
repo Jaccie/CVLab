3b3a65b096ec2828162708648f5fd3ccf9f272d3
JaccieKao <jacciekao@gmail.com>
2017-02-16 17:41:48 +0800

    gp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,18 +1,17 @@
-// ConsoleApplication1.cpp : ©w∏q•D±±•x¿≥•Œµ{¶°™∫∂i§J¬I°C
+Ôªø// ConsoleApplication1.cpp : ÂÆöÁæ©‰∏ªÊéßÂè∞ÊáâÁî®Á®ãÂºèÁöÑÈÄ≤ÂÖ•Èªû„ÄÇ
 //
 
 #include "stdafx.h"
-
-
-/*#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
-hihihihihihihih
+
 using namespace cv;
 using namespace std;
 
-int main(int argc, char* argv[])
+/*int main(int argc, char* argv[])
 {
-	VideoCapture cap("story.mp4"); // open the video file for reading
+	VideoCapture cap("video.mp4"); // open the video file for reading
+	VideoCapture capcam(0);
 
 	if (!cap.isOpened())  // if not success, exit program
 	{
@@ -23,6 +22,8 @@ int main(int argc, char* argv[])
 	//cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
 
 	double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
+	double Width = capcam.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+	double Height = capcam.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
 
 	cout << "Frame per seconds : " << fps << endl;
 
@@ -30,17 +31,18 @@ int main(int argc, char* argv[])
 
 	while (1)
 	{
-		Mat frame;
-
-		bool bSuccess = cap.read(frame); // read a new frame from video
-
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read the frame from video file" << endl;
-			break;
+		Mat frame, frame1;
+		cap >> frame;
+		capcam >> frame1;
+		 //show the frame in "MyVideo" window
+		for (int i = 0; i < Width; i++) {
+			for (int j = 0; j < Height; j++) {
+				frame.at<Vec3b>(j, i)[0] = frame1.at<Vec3b>(j, i)[0]; //B
+				frame.at<Vec3b>(j, i)[1] = frame1.at<Vec3b>(j, i)[1];//G
+				frame.at<Vec3b>(j, i)[2] = frame1.at<Vec3b>(j, i)[2];//R
+			}
 		}
-
-		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+		imshow("MyVideo", frame);
 
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
 		{
@@ -52,6 +54,7 @@ int main(int argc, char* argv[])
 	return 0;
 
 }
+*/
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 
@@ -97,8 +100,8 @@ int main(int argc, char* argv[])
 	}
 	return 0;
 
-}*/
-
+}
+/*
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 
@@ -107,18 +110,17 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	VideoCapture cap("story.mp4");  // open the video file for reading
-	VideoCapture cap1(0); 	        // open the video camera no. 0
-
-	if (!cap.isOpened())  // if not success, exit program
+	VideoCapture CapVideo("video .mp4");  // open the video file for reading
+	if (!CapVideo.isOpened())  // if not success, exit program
 	{
 		cout << "Cannot open the video file" << endl;
-		//return -1;
+		return -1;
 	}
-	if (!cap1.isOpened())  // if not success, exit program
+	VideoCapture CapCamera(0); 	        // open the video camera no. 0
+	if (!CapCamera.isOpened())  // if not success, exit program
 	{
 		cout << "Cannot open the video cam" << endl;
-		//return -1;
+		return -1;
 	}
 
 	//cap.set(CV_CAP_PROP_POS_MSEC, 300); //start the video at 300ms
@@ -131,21 +133,22 @@ int main(int argc, char* argv[])
 
 	while (1)
 	{
-		Mat frame;
+		Mat frameVid,frameCam;
 
-		bool bSuccess = cap.read(frame); // read a new frame from video
-		bool aSuccess = cap1.read(frame);
+		bool bSuccess = CapVideo.read(frameVid); // read a new frame from video
+		bool aSuccess = CapCamera.read(frameCam);
 
-		if (!bSuccess) 			 //if not success, break loop
+		if (!bSuccess || !aSuccess) 			 //if not success, break loop
 		{
 			cout << "Cannot read the frame from video file" << endl;
-			//break;
+			break;
 		}
 
-		imshow("VideoWindow", frame); //show the frame in "MyVideo" window
-		imshow("VideoWindow", frame);
+		imshow("VideoWindow", frameVid); //show the frame in "MyVideo" window
+		imshow("CameraWindow", frameCam);
 	}
 
 	return 0;
 
-}
+}*/
+
